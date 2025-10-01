6afc2e936121848c005f5c4d9cf1283ed9deb861
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-10-02 01:51:30 +0200

    Update cluster_controller.ts

--- back/app/controllers/cluster_controller.ts
+++ back/app/controllers/cluster_controller.ts
@@ -12,18 +12,22 @@ export default class ClusterController {
         const { minLat, maxLat, minLng, maxLng, zoom } = await request.validateUsing(getClustersValidator);
 
         let precision: number = 2;
-        if (zoom >= 3 && zoom < 6) {
+        if (zoom >= 2 && zoom < 4) {
+            precision = 2;
+        } else if (zoom >= 4 && zoom < 6) {
             precision = 3;
         } else if (zoom >= 6 && zoom < 8) {
             precision = 4;
         } else if (zoom >= 8 && zoom < 10) {
             precision = 5;
         } else if (zoom >= 10 && zoom < 12) {
-            precision = 7;
+            precision = 6;
         } else if (zoom >= 12 && zoom < 13) {
             precision = 7;
-        } else if (zoom >= 13) {
+        } else if (zoom >= 13 && zoom < 16) {
             precision = 8;
+        } else if (zoom >= 16) {
+            precision = 9;
         }
 
         const clusters: Address[] = await this.addressRepository.getClusters(minLat, maxLat, minLng, maxLng, precision);
